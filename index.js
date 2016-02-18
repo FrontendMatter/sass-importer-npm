f670078783692afa4191eb7e6ea2ab47c5440fb4
Laza Bogdan <laza.bogdan@macro-net.ro>
2016-02-19 01:10:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var fs = require('fs')
+var	path = require('path')
+var resolve = require('resolve');
+
+function resolvePath (r) {
+  for (var parent = module.parent; parent; parent = parent.parent) {
+    if (parent.paths && parent.paths.length && parent.paths.indexOf(path.join(process.cwd(), 'node_modules')) !== -1) {
+      return resolve.sync(r, { basedir: parent.paths[0] });
+    }
+  }
+  return require.resolve(r);
+}
+
+// Look for modules installed through npm
+// See https://github.com/sass/node-sass#importer--v200---experimental
+module.exports = function(url, file, done) {
+	if (url.indexOf('.scss') === -1) {
+		url += '.scss'
+	}
+
+	try {
+		url = resolvePath(url)
+	}
+	catch (e) {
+		// look for the same file prefixed with _
+		var chunks = url.split('/')
+		var filename = chunks.pop()
+		
+		if (filename.indexOf('_') === -1) {
+			try {
+				chunks.push('_' + filename)
+				url = resolvePath(chunks.join('/'))
+			}
+			catch (e) {}
+		}
+	}
+	return done({
+		file: url
+	})
+}
